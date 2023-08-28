1a936d619c3911608352ec5d087a3e08cd73d10e
thegreatcode1 <suhaibpa72@gmail.com>
2023-08-28 22:12:30 +0530

    initial commit for python learning

---
+++ sample.py
@@ -0,0 +1,15 @@
+# simple output
+print("hello world")
+
+# adding two numbers
+a = 10
+b = 20
+print(a+b)
+
+# concatination string and a number
+c = a+b
+print("sum of to numbers: "+str(c)+" is the sum of two no.")
+
+# get input from user
+d = input("enter a number or string")
+print("user input number or string"+str(d))
