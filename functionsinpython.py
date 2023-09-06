ffa8101dd9130c44bc970a16220eabfe9bd2ef81
thegreatcode1 <suhaibpa72@gmail.com>
2023-09-06 16:01:56 +0530

    functions

--- functionsinpython.py
+++ functionsinpython.py
@@ -13,3 +13,17 @@ def hello(name,age):
 # hello func call
 value='suhail'
 hello(value,25)
+
+# tuples
+tp=('hello',"haii","whatsapp...")
+print(tp[0])
+"""
+this is tuple it is similar to list but tuple are not allowed to modified 
+values tuple can only be permit to view the values
+"""
+
+# arbitrary argument
+def hello1(*values):
+  print("first: "+values[0]+" secound: "+values[1],values[2])
+
+hello1("haii","hello","how are you")  
