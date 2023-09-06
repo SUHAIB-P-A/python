4abe721e794debab51ea278618b39d864bc1f7b1
thegreatcode1 <suhaibpa72@gmail.com>
2023-09-06 15:40:37 +0530

    functions

---
+++ functionsinpython.py
@@ -0,0 +1,15 @@
+# function in python
+# function defnition
+def hey():
+  print("helo")
+
+# function call
+hey()
+
+# argument passing inn a function
+def hello(name):
+  print("my name is ",name)
+
+# hello func call
+value='suhail'
+hello(value)
