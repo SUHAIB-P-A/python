651135c6f2883f722f42bb2c0d80ae3e09b4add1
thegreatcode1 <suhaibpa72@gmail.com>
2023-09-09 14:52:20 +0530

    globel and local variables

--- functionsinpython.py
+++ functionsinpython.py
@@ -1,21 +1,25 @@
 # function in python
 # function defnition
 def hey():
-  print("helo")
+    print("helo")
+
 
 # function call
 hey()
 
 # argument passing inn a function
-def hello(name,age):
-  print("my name is ",name,'age:'+str(age))
+
+
+def hello(name, age):
+    print("my name is ", name, 'age:'+str(age))
+
 
 # hello func call
-value='suhail'
-hello(value,25)
+value = 'suhail'
+hello(value, 25)
 
 # tuples
-tp=('hello',"haii","whatsapp...")
+tp = ('hello', "haii", "whatsapp...")
 print(tp[0])
 """
 this is tuple it is similar to list but tuple are not allowed to modified 
@@ -23,7 +27,27 @@ values tuple can only be permit to view the values
 """
 
 # arbitrary argument
-def hello1(*values):
-  print("first: "+values[0]+" secound: "+values[1],values[2]) # values get in to the tuples
 
-hello1("haii","hello","how are you") # like a tuple & func call
+
+def hello1(*values):
+    print("first: "+values[0]+" secound: "+values[1],
+          values[2])  # values get in to the tuples
+
+
+hello1("haii", "hello", "how are you")  # like a tuple & func call
+
+
+# concept of GLOBAL variable & LOCAL variable
+val = 10  # this is the globel variable
+
+def sample():
+    val = 30  # it is a local variable
+    val = val+1  # here  an error 'val+1' val is not resolved in this error will resolve we use 'val = 30'
+    s = val+1# another methord 
+    # the above three lines are local variables these are not accessed other part of the code 
+    print(val)  # in this variable can be accesed any way in the whole code
+    # here is the 1st example val variable used in to the sample func
+
+
+print(val)  # here is the 2ed example using outside of the sample func
+sample()
