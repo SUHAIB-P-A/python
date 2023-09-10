d818e1b8a444a8b09883d03d45819db6b389cfc9
thegreatcode1 <suhaibpa72@gmail.com>
2023-09-10 20:57:07 +0530

    import use example

---
+++ import example/import.py
@@ -0,0 +1,5 @@
+import sum # this is the example for import a file in another file
+# the below code is function call of the 'summasion func'
+sum.summasion(num1=2,num2=3)
+# after import a file it will run all type code in that sum file
+# example is the two print func in sum file
---
+++ import example/sum.py
@@ -0,0 +1,9 @@
+print('hello')
+
+def summasion(num1,num2):# this is a sample sum code in this code 
+     # the func summasion call in another file using 'import'
+     # then it will shows that 'import.py file'
+     sum=num1+num2
+     print(sum)
+
+print('suhail')     
