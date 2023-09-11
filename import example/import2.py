9c8cc74cfdf7d895905b2d1b019a4bf8bffb925a
thegreatcode1 <suhaibpa72@gmail.com>
2023-09-11 11:30:30 +0530

    function name change stratin

--- import example/import2.py
+++ import example/import2.py
@@ -3,12 +3,14 @@
 # so in that case we will write it as
 
 from sum import summasion,printwelcome# from which module/file import which func we get all the func in the file separated using ','
+from sum import printwelcome as welcome
 
 summasion(2,3)
 result=summasion
 result(20,30)
 
-printwelcome()
+printwelcome()# print 'welcome'
+welcome()# print 'welcome'
 
 # errors
 #sum.summasion(2,3)# it is  an errorbecause here not use the all module
