e705171e36bc49a6511ed75a950079d4759bd59a
thegreatcode1 <suhaibpa72@gmail.com>
2023-09-11 11:53:55 +0530

    in-build modiles in pythom

---
+++ import example/default_py_modules.py
@@ -0,0 +1,12 @@
+
+#  modules in python
+# 1
+import platform
+
+print(platform.python_compiler())
+print(platform.machine())
+print(platform.processor())
+print(platform.system())
+print(platform.release())
+print(platform.node())
+print(platform.python_version())
