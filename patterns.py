97af04624d4e6bd257c1f7004bb3e60db272a449
thegreatcode1 <suhaibpa72@gmail.com>
2023-09-15 16:12:29 +0530

    nuw pattern

--- patterns.py
+++ patterns.py
@@ -4,23 +4,23 @@
   * *
  * * *  
 """  # code for this pattern
-n=int(input('enter the no of rows'))
-i=1
-while i<=n:
-    j=1
-    while j<=n-i:
-       print('',end=" ")
-       j+=1
+n = int(input('enter the no of rows'))
+i = 1
+while i <= n:
+    j = 1
+    while j <= n-i:
+        print('', end=" ")
+        j += 1
 
-    j=1
-    while j<=i:
-      print('*',end=" ")
-      j=j+1
+    j = 1
+    while j <= i:
+        print('*', end=" ")
+        j = j+1
 
     print('')
-    i=i+1
-    
-    
+    i = i+1
+
+
 # problem
 
 n = 5  # Number of rows
@@ -39,7 +39,7 @@ while i <= n:
         print(j, end=" ")
         j += 1
 
-    #Print numbers in descending order (excluding 1)
+    # Print numbers in descending order (excluding 1)
     j = i - 1
     while j >= 1:
         print(j, end=" ")
@@ -57,18 +57,51 @@ while i <= n:
    *
 """
 
-n=int(input('enter the no of rows'))
-i=1
-while i<=n:
-    j=1
-    while j<=i:
-       print('',end=" ")
-       j+=1
+n = int(input('enter the no of rows'))
+i = 1
+while i <= n:
+    j = 1
+    while j <= i:
+        print('', end=" ")
+        j += 1
 
-    j=n
-    while j>=i:
-      print('*',end=" ")
-      j=j-1
+    j = n
+    while j >= i:
+        print('*', end=" ")
+        j = j-1
 
     print('')
-    i=i+1
+    i = i+1
+
+
+# problem
+
+"""
+*
+* *
+* * *
+* *
+*
+"""
+
+n = int(input('enter the no of rows'))
+# first half +1
+i = 1
+while i <= (n/2)+1:
+    j = 1
+    while j <= i:
+        print('*', end=" ")
+        j = j+1
+
+    print('')
+    i = i+1
+# secound half
+i = 1
+while i <= (n/2):
+    j = (n/2)
+    while j >= i:
+        print('*', end=" ")
+        j = j-1
+
+    print('')
+    i = i+1
