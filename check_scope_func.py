b2851968ebde507865dcd0a0917c2e727000f154
thegreatcode1 <suhaibpa72@gmail.com>
2023-09-17 05:31:12 +0530

    nonlocal keyword

--- check_scope_func.py
+++ check_scope_func.py
@@ -6,6 +6,8 @@ def check_scope():
         print(test)
 
     def do_non_local():
+        nonlocal test
+        print(test)  # output is 'default'
         test = 'non loacal test'
 
     def do_globel():
@@ -14,7 +16,12 @@ def check_scope():
     test = 'default'
     do_local()
     print('after calling do_local is: '+test)
-# output is 'after calling do_local is: default'
+    # output is 'after calling do_local is: default'
+
+    do_non_local()  # in this case nonlocal keyword check_scope define test variable pass to the do_non_local func then it is change the test variable value 'default'-->'non loacal test' proof is do_non_local variable print
+    # that test variable is print it
+    print('after calling do_non_local is: '+test)
+    # output is 'after calling do_non_local is: non loacal test'
 
 
 check_scope()  # first call func --> call do_local func --> test value is 'local test' --> after executing the code test value will replaced 'default'--> after printing default the execution end
