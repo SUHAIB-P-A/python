da6c0f7849be744771136999fa7b1054370603ea
thegreatcode1 <suhaibpa72@gmail.com>
2023-09-17 10:05:35 +0530

    parameter passing on the class func

--- Oops/sampleclass.py
+++ Oops/sampleclass.py
@@ -1,14 +1,15 @@
 # class definition
 class Mysampleclass:
     # func definision
-    def hello():  # in other python doc in class inside func contain a 'self' parameter but here what ever it is not mantatory
-        print("hello")
+    def hello(name):  # in other python doc in class inside func contain a 'self' parameter but here what ever it is not mantatory
+        print("hello "+name)
     print("haii")
 
 
 # object of the class
 x = Mysampleclass
 # function calling inside the class methord 1
-x.hello()
+name='suhaib'
+x.hello(name)# parameter passing
 # function calling inside the class methord 2
-Mysampleclass.hello()
+Mysampleclass.hello(name)
