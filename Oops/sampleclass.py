63b0123455ea719595ec7e63fb70836cc6720777
thegreatcode1 <suhaibpa72@gmail.com>
2023-09-17 06:25:52 +0530

    Oops start creation of class and object of the class

---
+++ Oops/sampleclass.py
@@ -0,0 +1,14 @@
+# class definition
+class Mysampleclass:
+    # func definision
+    def hello():
+        print("hello")
+    print("haii")
+
+
+# object of the class
+x = Mysampleclass
+# function calling inside the class methord 1
+x.hello()
+# function calling inside the class methord 2
+Mysampleclass.hello()
