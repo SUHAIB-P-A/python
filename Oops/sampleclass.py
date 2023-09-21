1d19034c256e523f298b0b16d61c8c34fa870a76
thegreatcode1 <suhaibpa72@gmail.com>
2023-09-22 05:13:10 +0530

    self obj uses

--- Oops/sampleclass.py
+++ Oops/sampleclass.py
@@ -1,15 +1,30 @@
 # class definition
 class Mysampleclass:
     # func definision
-    def hello(name):  # in other python doc in class inside func contain a 'self' parameter but here what ever it is not mantatory
+    def hello(self, name, addr):  # in other python doc in class inside func contain a 'self' parameter but here what ever it is not mantatory
+        self.names = name  # self means obj of the class that store the value in different variables
+        self.names1 = 'haii hello'
+        self.address = addr
         print("hello "+name)
+
+    def print_name(self):
+        # we will create a another func and it will print using self obj
+        print(self.names)
+        print(self.names1)
+        print(self.address)
     print("haii")
 
 
 # object of the class
 x = Mysampleclass
 # function calling inside the class methord 1
-name='suhaib'
-x.hello(name)# parameter passing
+name = 'suhail'
+address = """
+paraalahayaur,
+trssur,
+keaa
+"""
+x.hello(x, name, address)  # parameter passing
 # function calling inside the class methord 2
-Mysampleclass.hello(name)
+Mysampleclass.hello(x, name, address)
+x.print_name(x)  # calling of the func
