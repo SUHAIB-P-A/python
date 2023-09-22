d65809200ac20f614018f93be5065019174c6ccd
thegreatcode1 <suhaibpa72@gmail.com>
2023-09-22 05:31:45 +0530

    self obj problem/bug

--- Oops/sampleclass.py
+++ Oops/sampleclass.py
@@ -3,20 +3,21 @@ class Mysampleclass:
     # func definision
     def hello(self, name, addr):  # in other python doc in class inside func contain a 'self' parameter but here what ever it is not mantatory
         self.names = name  # self means obj of the class that store the value in different variables
-        self.names1 = 'haii hello'
+        #self.names1 = 'haii hello'
         self.address = addr
-        print("hello "+name)
+       # print("hello "+name)
 
     def print_name(self):
         # we will create a another func and it will print using self obj
         print(self.names)
-        print(self.names1)
+       # print(self.names1)
         print(self.address)
-    print("haii")
+   # print("haii")
 
 
 # object of the class
 x = Mysampleclass
+y = Mysampleclass
 # function calling inside the class methord 1
 name = 'suhail'
 address = """
@@ -24,7 +25,10 @@ paraalahayaur,
 trssur,
 keaa
 """
-x.hello(x, name, address)  # parameter passing
+
+y.hello(y,name,address)
+x.hello(x, "suhail lll", address)  # parameter passing
 # function calling inside the class methord 2
-Mysampleclass.hello(x, name, address)
+#Mysampleclass.hello(x, name, address)
 x.print_name(x)  # calling of the func
+y.print_name(y)
