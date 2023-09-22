bec3c3500e6ae178e1166c5ad5362c68ef4bd3e8
thegreatcode1 <suhaibpa72@gmail.com>
2023-09-22 05:50:59 +0530

    self ueing problem value not change

--- Oops/sampleclass.py
+++ Oops/sampleclass.py
@@ -1,17 +1,17 @@
 # class definition
 class Mysampleclass:
     # func definision
-    def hello(self, name, addr):  # in other python doc in class inside func contain a 'self' parameter but here what ever it is not mantatory
+    def hello(self, name):  # in other python doc in class inside func contain a 'self' parameter but here what ever it is not mantatory
         self.names = name  # self means obj of the class that store the value in different variables
-        #self.names1 = 'haii hello'
-        self.address = addr
+        # self.names1 = 'haii hello'
+
        # print("hello "+name)
 
     def print_name(self):
         # we will create a another func and it will print using self obj
         print(self.names)
        # print(self.names1)
-        print(self.address)
+
    # print("haii")
 
 
@@ -20,15 +20,10 @@ x = Mysampleclass
 y = Mysampleclass
 # function calling inside the class methord 1
 name = 'suhail'
-address = """
-paraalahayaur,
-trssur,
-keaa
-"""
 
-y.hello(y,name,address)
-x.hello(x, "suhail lll", address)  # parameter passing
+y.hello(y, name)
+x.hello(x, "suhail lll")  # parameter passing
 # function calling inside the class methord 2
-#Mysampleclass.hello(x, name, address)
+# Mysampleclass.hello(x, name, address)
 x.print_name(x)  # calling of the func
 y.print_name(y)
