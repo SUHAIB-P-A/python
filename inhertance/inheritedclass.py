a10599658efe75ae96f32134ed1b6f794e79e7c5
thegreatcode1 <suhaibpa72@gmail.com>
2023-10-20 21:54:42 +0530

    constructor overreading

--- inhertance/inheritedclass.py
+++ inhertance/inheritedclass.py
@@ -1,18 +1,25 @@
 class base:
+    def __init__(self):  # sinple constarctor
+        print("base init")
+
     def set_name(self, name):
         self.name = name
 
 
 class subclass(base):  # subclass inherited base a copy of base inside the subclass
-    def print_welcom():
+
+    def __init__(self):  # constroctor overreading example
+        print("subclass init")
+
+    def print_welcom(self):
         print("welcom")
 
     def dispaly_name(self):
         print(self.name)
 
 
-x = subclass  # object creation
+x = subclass()  # object creation
 
 x.print_welcom()
-x.set_name(x, "suhail")
-x.dispaly_name(x)
+x.set_name("suhail")
+x.dispaly_name()
