0ec76da791ae45c4326b93900f28a256815426ff
thegreatcode1 <suhaibpa72@gmail.com>
2023-10-22 10:09:33 +0530

    create a sample button

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,5 +1,13 @@
 from tkinter import *
 
-window=Tk()
+window=Tk()# object 
+
+window.geometry("500x500")
+window.config(bg="#0e493e")
+# buttons
+button1=Button(window,text="ok")
+
+button1.pack()
+
 
 window.mainloop()
