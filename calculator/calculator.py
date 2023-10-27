9abc37127297dce7efebb6e55fc68d69e50e2ea7
thegreatcode1 <suhaibpa72@gmail.com>
2023-10-27 21:14:22 +0530

    change color

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -6,7 +6,7 @@ from PIL import Image, ImageTk
 window = tk.Tk()  # object
 
 window.geometry("400x600")
-window.config(bg="#0e493e")
+window.config(bg="#e0ffff")
 window.title("calculator")
 # icon= tk.PhotoImage(r"C:/Users/Suhaib/learnPy/calculator/calculator_jP3_icon.ico")
 # image load using PIL
@@ -19,34 +19,33 @@ window.tk.call('wm', 'iconphoto', window._w, '-default', icon)
 
 
 def Button_click(text):
-    if(text=="C"):
+    if (text == "C"):
         clear_screen()
-    elif(text=="="):
+    elif (text == "="):
         result()
     else:
-        append_screen(text)        
-    
+        append_screen(text)
+
 
 def clear_screen():
-    calculator_screen.delete(0,tk.END) 
+    calculator_screen.delete(0, tk.END)
+
 
 def result():
     try:
-        result=str(eval(calculator_screen.get()))
-        calculator_screen.delete(0,tk.END)
-        calculator_screen.insert(0,result)
+        result = str(eval(calculator_screen.get()))
+        calculator_screen.delete(0, tk.END)
+        calculator_screen.insert(0, result)
     except EXCEPTION:
-        calculator_screen.delete(0,tk.END)
-        calculator_screen.insert(0,"error")         
+        calculator_screen.delete(0, tk.END)
+        calculator_screen.insert(0, "error")
+
 
 def append_screen(text):
     current_text = calculator_screen.get()
     calculator_screen.delete(0, tk.END)
     calculator_screen.insert(0, current_text+text)
     print(current_text)
-
-
-
 
 
 # TextBox
@@ -58,23 +57,38 @@ calculator_screen.grid(row=0, column=0, columnspan=4,
 
 f = font.Font(size=20)
 # buttons
-button1 = Button(window, text="1", font=f, command=lambda: Button_click("1"))
-button2 = Button(window, text="2", font=f, command=lambda: Button_click("2"))
-button3 = Button(window, text="3", font=f, command=lambda: Button_click("3"))
-button4 = Button(window, text="4", font=f, command=lambda: Button_click("4"))
-button5 = Button(window, text="5", font=f, command=lambda: Button_click("5"))
-button6 = Button(window, text="6", font=f, command=lambda: Button_click("6"))
-button7 = Button(window, text="7", font=f, command=lambda: Button_click("7"))
-button8 = Button(window, text="8", font=f, command=lambda: Button_click("8"))
-button9 = Button(window, text="9", font=f, command=lambda: Button_click("9"))
-button0 = Button(window, text="0", font=f, command=lambda: Button_click("0"))
-buttonok = Button(window, text="=", font=f, command=lambda: Button_click("="))
-buttondot = Button(window, text="C", font=f, command=lambda: Button_click("C"))
-buttonplus = Button(window, text="+", font=f,
+button1 = Button(window, text="1", font=f, bg='#00e4f5',
+                 command=lambda: Button_click("1"))
+button2 = Button(window, text="2", font=f, bg='#00e4f5',
+                 command=lambda: Button_click("2"))
+button3 = Button(window, text="3", font=f, bg='#00e4f5',
+                 command=lambda: Button_click("3"))
+button4 = Button(window, text="4", font=f, bg='#00e4f5',
+                 command=lambda: Button_click("4"))
+button5 = Button(window, text="5", font=f, bg='#00e4f5',
+                 command=lambda: Button_click("5"))
+button6 = Button(window, text="6", font=f, bg='#00e4f5',
+                 command=lambda: Button_click("6"))
+button7 = Button(window, text="7", font=f, bg='#00e4f5',
+                 command=lambda: Button_click("7"))
+button8 = Button(window, text="8", font=f, bg='#00e4f5',
+                 command=lambda: Button_click("8"))
+button9 = Button(window, text="9", font=f, bg='#00e4f5',
+                 command=lambda: Button_click("9"))
+button0 = Button(window, text="0", font=f, bg='#00e4f5',
+                 command=lambda: Button_click("0"))
+buttonok = Button(window, text="=", font=f, bg='#00e4f5',
+                  command=lambda: Button_click("="))
+buttondot = Button(window, text="C", font=f, bg='#00e4f5',
+                   command=lambda: Button_click("C"))
+buttonplus = Button(window, text="+", font=f, bg='#00e4f5',
                     command=lambda: Button_click("+"))
-buttonsub = Button(window, text="-", font=f, command=lambda: Button_click("-"))
-buttondiv = Button(window, text="/", font=f, command=lambda: Button_click("/"))
-buttonmul = Button(window, text="*", font=f, command=lambda: Button_click("*"))
+buttonsub = Button(window, text="-", font=f, bg='#00e4f5',
+                   command=lambda: Button_click("-"))
+buttondiv = Button(window, text="/", font=f, bg='#00e4f5',
+                   command=lambda: Button_click("/"))
+buttonmul = Button(window, text="*", font=f, bg='#00e4f5',
+                   command=lambda: Button_click("*"))
 
 # arrange the button(matrix or grid)
 button7.grid(row=1, column=0, padx=10, pady=10, ipadx=20, ipady=20)
